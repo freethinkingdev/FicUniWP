027f55e776b4e9fcd8c7d78255a9f12d1633bb73
Marcin W <emwu83@gmail.com>
2017-11-27 17:58:08 +0000

    Updated some of the links for the a tags.

--- wp-content/themes/dark-master/footer.php
+++ wp-content/themes/dark-master/footer.php
@@ -20,7 +20,7 @@ wp_footer();
         <div class="group">
 
             <div class="site-footer__col-one">
-                <h1 class="school-logo-text school-logo-text--alt-color"><a href="#"><strong>Fictional</strong> University</a></h1>
+                <h1 class="school-logo-text school-logo-text--alt-color"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
                 <p><a class="site-footer__link" href="#">555.555.5555</a></p>
             </div>
 
@@ -29,10 +29,10 @@ wp_footer();
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
                         <ul>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Programs</a></li>
-                            <li><a href="#">Events</a></li>
-                            <li><a href="#">Campuses</a></li>
+                            <li><a href="<?php echo site_url('about-us'); ?>">About Us</a></li>
+                            <li><a href="<?php echo site_url('programs'); ?>">Programs</a></li>
+                            <li><a href="<?php echo site_url('events'); ?>">Events</a></li>
+                            <li><a href="<?php echo site_url('campuses'); ?>">Campuses</a></li>
                         </ul>
                     </nav>
                 </div>
