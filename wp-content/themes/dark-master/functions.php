91abc8fbf3688fcb9291a6309e11c59c00491f48
Marcin W <emwu83@gmail.com>
2017-11-27 17:57:50 +0000

    Added site tites that show up on the browser tab. Added css and js files.

--- wp-content/themes/dark-master/functions.php
+++ wp-content/themes/dark-master/functions.php
@@ -8,10 +8,16 @@
 
 
 add_action('wp_enqueue_scripts', 'uni_files');
+add_action('after_setup_theme','uni_features');
+
 
 function uni_files() {
-    wp_enqueue_script('slider_code',get_theme_file_uri('/js/scripts-bundled.js'),NULL,'1.0',true);
+    wp_enqueue_script('slider_code',get_theme_file_uri('/js/scripts-bundled.js'),NULL,microtime(),true);
     wp_enqueue_style('google_font','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('uni_main_css',get_stylesheet_uri());
+    wp_enqueue_style('uni_main_css',get_stylesheet_uri(),null,microtime(),false);
+}
+
+function uni_features() {
+    add_theme_support('title-tag');
 }
