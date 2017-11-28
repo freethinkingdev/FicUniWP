72cacd449bff2fa0900876ecd56bb220541fdd5d
Marcin W <emwu83@gmail.com>
2017-11-28 15:39:48 +0000

    Added archive page template to show given author or category posts.

---
+++ wp-content/themes/dark-master/archive.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcin
+ * Date: 28/11/2017
+ * Time: 15:02
+ */
+
+
+get_header();
+
+?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri() ?>/images/ocean.jpg);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+            <div class="page-banner__intro">
+                <?php //TODO::Change into subtitle ?>
+                <p><?php the_archive_description(); ?></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        <?php
+        while (have_posts()) {
+            the_post();
+
+            ?>
+            <div class="post-item">
+                <!-- Post title that is also a link-->
+                <h2 class="headline headline--medium headline-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="metabox">
+                    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j M, y'); ?> in <?php echo get_the_category_list(' and '); ?></p>
+                </div>
+
+
+                <div class="generic-content">
+                    <?php the_excerpt(); ?>
+                    <p><a class="btn btn--gray" href="<?php the_permalink(); ?>">Continue to read &rightarrow;</a> </p>
+                </div>
+
+            </div>
+            <?php
+
+        }
+        echo paginate_links();
+        ?>
+    </div>
+
+<?php
+get_footer();
