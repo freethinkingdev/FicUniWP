2cf253171b89b94e569e43f33e4ceb80201ce7c9
Marcin W <emwu83@gmail.com>
2017-11-28 08:55:27 +0000

    Updated single template for the page. Added banner on top and created place for conten to show up.

--- wp-content/themes/dark-master/single.php
+++ wp-content/themes/dark-master/single.php
@@ -7,16 +7,34 @@
  */
 
 get_header();
+?>
 
-while (have_posts()) {
-    the_post();
-    ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri() ?>/images/ocean.jpg);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <?php //TODO::Change into subtitle ?>
+                <p>Latest news</p>
+            </div>
+        </div>
+    </div>
 
-    <h1><?php the_title() ?></h1>
-    <p><?php the_content() ?></p>
+    <div class="container container--narrow page-section">
+        <?php
+        while (have_posts()) {
+            the_post();
 
-    <a href="<?php echo site_url(); ?>">Back</a>
-    <?php
-}
+            ?>
+            <div class="post-item">
+                <p><?php the_content(); ?></p>
 
+            </div>
+            <?php
+
+        }
+        ?>
+    </div>
+
+<?php
 get_footer();
