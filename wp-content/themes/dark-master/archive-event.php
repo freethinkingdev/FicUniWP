0096615b59741b249e4256057bebdf2a7704ec0d
Marcin W <emwu83@gmail.com>
2017-11-28 23:05:35 +0000

    Added template files for the event post type: archive and single.

---
+++ wp-content/themes/dark-master/archive-event.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcin
+ * Date: 28/11/2017
+ * Time: 22:50
+ */
+
+
+get_header();
+
+?>
+
+
+    <div class="page-banner" xmlns="http://www.w3.org/1999/html">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_theme_file_uri() ?>/images/ocean.jpg);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>Upcoming events...</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+
+        <?php
+        $events_for_the_home_page = new WP_Query(array(
+            'posts_per_page' => 10,
+            'post_type' => 'event'
+        ));
+
+        while ($events_for_the_home_page->have_posts()) {
+            $events_for_the_home_page->the_post();
+
+            ?>
+            <div class="generic-content">
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month"><?php the_time('M'); ?></span>
+                    <span class="event-summary__day"><?php the_time('d'); ?></span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a
+                            href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php echo wp_trim_words(get_the_content(), 20); ?>
+                        <a href="<?php the_permalink(); ?>" class="nu gray">Learn more &raquo;</a></p>
+                </div>
+
+            </div>
+            </div>
+
+
+        <?php } ?>
+        <?php
+
+        echo paginate_links();
+        ?>
+    </div>
+
+<?php
+get_footer();
---
+++ wp-content/themes/dark-master/single-event.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcin
+ * Date: 28/11/2017
+ * Time: 22:29
+ */
+
+get_header();
+?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri() ?>/images/ocean.jpg);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <?php //TODO::Change into subtitle ?>
+                <p>Latest news</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+        <div class="metabox metabox--position-up metabox--with-home-link">
+            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Event Home</a>
+            <span class="metabox__main"><?php the_title(); ?></span></p>
+        </div>
+        <div class="generic-content">
+            <?php
+            while (have_posts()) {
+                the_post();
+
+                ?>
+                <div class="post-item">
+                    <p><?php the_content(); ?></p>
+
+                </div>
+                <?php
+
+            }
+            ?>
+        </div>
+
+
+    </div>
+
+<?php
+get_footer();
