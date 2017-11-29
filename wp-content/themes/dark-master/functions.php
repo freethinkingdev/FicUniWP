b0e78629c3d4e8ace3e347a52314909d9072f003
Marcin W <emwu83@gmail.com>
2017-11-29 17:08:28 +0000

    Added sorting functionality.

--- wp-content/themes/dark-master/functions.php
+++ wp-content/themes/dark-master/functions.php
@@ -8,19 +8,43 @@
 
 
 add_action('wp_enqueue_scripts', 'uni_files');
-add_action('after_setup_theme','uni_features');
+add_action('after_setup_theme', 'uni_features');
+add_action('pre_get_posts', 'uni_events_list');
 
 
-function uni_files() {
-    wp_enqueue_script('slider_code',get_theme_file_uri('/js/scripts-bundled.js'),NULL,microtime(),true);
-    wp_enqueue_style('google_font','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font_awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('uni_main_css',get_stylesheet_uri(),null,microtime(),false);
+/*Adding font awesome, js file, css and google font*/
+function uni_files()
+{
+    wp_enqueue_script('slider_code', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+    wp_enqueue_style('google_font', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('uni_main_css', get_stylesheet_uri(), null, microtime(), false);
 }
 
-function uni_features() {
+/*Adding menus to the site*/
+function uni_features()
+{
     add_theme_support('title-tag');
     register_nav_menu('menu_header', 'Header menu location');
     register_nav_menu('menu_footer_left', 'Footer left menu location');
     register_nav_menu('menu_footer_right', 'Footer right menu location');
+}
+
+/*Adjusting events on the archive-event page*/
+function uni_events_list($query)
+{
+    $day_today = date('Ymd');
+    if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
+        /*$query->set('posts_per_page', '2');*/
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            'key' => 'event_date',
+            'compare' =>'>=',
+            'value' => $day_today,
+            'type' => 'numeric'
+        ));
+
+    }
 }
