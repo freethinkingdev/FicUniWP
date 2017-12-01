1f0f13f6c67a21e71b50b9108ba578786c3ccc6f
Marcin W <emwu83@gmail.com>
2017-12-01 14:28:44 +0000

    Registered new campus and program custom post types. REmoved some empty lines.

--- wp-content/mu-plugins/uni_post_types.php
+++ wp-content/mu-plugins/uni_post_types.php
@@ -7,9 +7,7 @@
  */
 
 /*Adding new post type*/
-
 add_action('init','uni_post_types');
-
 function uni_post_types(){
     register_post_type('event', array(
         'public' => true,
@@ -22,6 +20,41 @@ function uni_post_types(){
             'all_items' => 'All Events',
             'singular_name' => 'Event'
         ),
-        'menu_icon' => 'dashicons-calendar-alt'
+        'menu_icon' => 'dashicons-calendar-alt',
+        'has_archive' => true,
+        'rewrite' => array('slug'=>'events'),
+        'supports' => array('title','editor','author','thumbnail','excerpt','trackbacks', 'revisions','post-formats')
+    ));
+    register_post_type('campus', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Campuses',
+            'add_new_item' => 'Add New Campus',
+            'edit_item' => 'Edit Campus',
+            'view_item' => 'View Campus',
+            'search_items' => 'Search Campuses',
+            'all_items' => 'All Campuses',
+            'singular_name' => 'Campus'
+        ),
+        'menu_icon' => 'dashicons-building',
+        'has_archive' => true,
+        'supports' => array('title','editor'),
+        'rewrite' => array('slug'=> 'campuses')
+    ));
+    register_post_type('program', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'view_item' => 'View Program',
+            'search_items' => 'Search Programs',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards',
+        'has_archive' => true,
+        'supports' => array('title','editor'),
+        'rewrite' => array('slug'=> 'programs')
     ));
 }
