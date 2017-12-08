0669dff9d4565e76da899496c469b5d65641565b
Marcin W <emwu83@gmail.com>
2017-12-08 22:02:49 +0000

    Added keyboard key events for s and esc to open and close the search box.

--- wp-content/themes/dark-master/js/SearchClass.js
+++ wp-content/themes/dark-master/js/SearchClass.js
@@ -10,6 +10,7 @@ class Search {
         this.search_button = $('.js-search-trigger');
         this.close_button = $('.close_button');
         this.search_overlay = $('.search-overlay');
+        this.searchBox = false;
         this.event();
     }
 
@@ -17,16 +18,42 @@ class Search {
     event(){
         this.search_button.on("click", this.open_search_window.bind(this));
         this.close_button.on("click", this.close_search_window.bind(this));
+        $(document).on('keydown',this.keyboard_key_listener_open_close_search_window.bind(this).bind(event));
     }
-    /*Methods description*/
+
+    /*Method that allows to open or close search div with the keyboard*/
+    keyboard_key_listener_open_close_search_window(event){
+        /*Setting keycode*/
+        var key_code = event.keyCode;
+
+        /*Checking which button has be pressed*/
+        switch(key_code) {
+            case 83:
+                if (!this.searchBox) {
+                    this.open_search_window();
+                }
+                break;
+            case 27:
+                if (this.searchBox){
+                    this.close_search_window();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    /*Open search window*/
     open_search_window(){
         this.search_overlay.addClass("search-overlay--active");
         $('body').addClass('body-no-scroll');
+        this.searchBox=true;
     }
 
+    /*Method that hides search window*/
     close_search_window(){
         this.search_overlay.removeClass('search-overlay--active');
         $('body').removeClass('body-no-scroll');
+        this.searchBox=false;
     }
 
 
