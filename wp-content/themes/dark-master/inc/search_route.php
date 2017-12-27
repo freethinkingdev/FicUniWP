47d7d81265ff1fadd0e4d5bf3e00a0d684ccb0f2
Marcin Wilinski <emwu83@gmail.com>
2017-12-27 16:37:26 +0000

    Added some comments. Updated arrays. Removed hardcoded values from the searched query.

--- wp-content/themes/dark-master/inc/search_route.php
+++ wp-content/themes/dark-master/inc/search_route.php
@@ -11,8 +11,17 @@
 add_action('rest_api_init', 'uni_custom_json_rest_response_when_searching');
 function uni_custom_json_rest_response_when_searching()
 {
+    /*Specifying json url that will be used in the process of the search*/
     register_rest_route('uni/v1', 'search', array(
+        /*
+         *Defines which HTTP methods are to be processed by the function defined by ‘callback’
+         * WP_REST_Server::READABLE = ‘GET’
+         * WP_REST_Server::EDITABLE = ‘POST, PUT, PATCH’
+         * WP_REST_Server::DELETABLE = ‘DELETE’
+         * WP_REST_Server::ALLMETHODS = ‘GET, POST, PUT, PATCH, DELETE’
+         */
         'methods' => WP_REST_Server::READABLE,
+        /*Method that runs as callback*/
         'callback' => 'uni_search_returned_results'
     ));
 }
@@ -26,11 +35,13 @@ function uni_search_returned_results($searched_item)
 {
     /*Getting custom wp_query in order to search for the item.*/
     $searched_results = new WP_Query(array(
+            /*Adding post types to look within when the search is performed*/
             'post_type' => array('program', 'post', 'event', 'page', 'professor'),
+            /*Specifying search query term*/
             's' => sanitize_text_field($searched_item['q'])
         )
     );
-    /*Creating multidimensional array with possible results categories*/
+    /*Creating multidimensional array with possible results categories that represent search custom posts*/
     $results_data_array = array(
         'post' => array(),
         'event' => array(),
@@ -39,12 +50,10 @@ function uni_search_returned_results($searched_item)
         'professor' => array()
     );
 
-
     /*Getting the loop in answer for the custom query*/
     while ($searched_results->have_posts()) {
         $searched_results->the_post();
         $post_type = get_post_type();
-
         /*Catching the post type and accordingly pushing data to the multidimensional array*/
         switch ($post_type) {
             /*The same feed goes for the page and post type post*/
@@ -59,7 +68,7 @@ function uni_search_returned_results($searched_item)
                     )
                 );
                 break;
-
+            /*In the case the post is an event*/
             case 'event':
                 $event_future_date = new DateTime(get_field('event_date'));
                 $excerpt_text = null;
@@ -80,7 +89,7 @@ function uni_search_returned_results($searched_item)
                     )
                 );
                 break;
-
+            /*In the case the post is the professor type*/
             case 'professor':
                 array_push($results_data_array['professor'], array(
                         'id' => get_the_ID(),
@@ -92,8 +101,7 @@ function uni_search_returned_results($searched_item)
                     )
                 );
                 break;
-
-
+            /*If the custom post type is the program*/
             case 'program':
                 array_push($results_data_array['program'], array(
                         'id' => get_the_ID(),
@@ -113,13 +121,15 @@ function uni_search_returned_results($searched_item)
         'post_type' => 'professor',
         /*Since we look in custom fields we use meta query*/
         'meta_query' => array(
+            /*Setting up relation to the OR. This would allow at least one filter to be true instead of them all*/
+            'relation' => 'OR',
             array(
                 /*Name of the advanced custom filed we want to look within*/
                 'key' => 'related_programs',
                 /*Way of comparing. LIKE represents SQL operator*/
                 'compare' => 'LIKE',
                 /*Value we look for*/
-                'value' => '"149"'
+                'value' => '"' . $results_data_array['program'][0]['id'] . '"'
             )
         )
     ));
@@ -129,14 +139,14 @@ function uni_search_returned_results($searched_item)
         $related_professor_custom_query->the_post();
         /*Populating professors array*/
 //        if(get_post_type() == 'professor'){
-            array_push($results_data_array['professor'], array(
-                'id' => get_the_ID(),
-                'title' => get_the_title(),
-                'permalink' => get_the_permalink(),
-                'author_name' => get_the_author(),
-                'type' => get_post_type(),
-                'image' => get_the_post_thumbnail(0, 'professor_portrait')
-            ));
+        array_push($results_data_array['professor'], array(
+            'id' => get_the_ID(),
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'author_name' => get_the_author(),
+            'type' => get_post_type(),
+            'image' => get_the_post_thumbnail(0, 'professor_portrait')
+        ));
 //        }
     }
     /*Removing duplicated from the array*/
